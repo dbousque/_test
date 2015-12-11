c048a169c5298c363a3652524d95cdbdce57f9e4
dodo <dodo@dodos-Mac-Pro.local>
2015-12-11 10:48:22 -0800

    lol3

--- fdf/ft_render_mesh.c
+++ fdf/ft_render_mesh.c
@@ -83,7 +83,7 @@ double		ft_get_min(t_mlx *mlx)
 	return (min);
 }
 
-void		ft_get_start_point(double *x_start, double *y_start, t_mlx *mlx, t_vector *dev)
+void		ft_get_start_point(double *x_start, double *y_start, t_mlx *mlx, t_vector *dev, t_vector *dev_3d)
 {
 	int		tab_height;
 	int		tab_width;
@@ -92,7 +92,7 @@ void		ft_get_start_point(double *x_start, double *y_start, t_mlx *mlx, t_vector 
 	tab_width = mlx->mesh[0][0];
 	tab_height = ft_nb_lines(mlx->mesh);
 
-	tmp_pnt = ft_new_point(WIDTH / 2 + ((tab_height - 1) / 2.0 * dev->y_step),
+	tmp_pnt = ft_new_point(WIDTH / 2 + ((tab_height - 1) / 2.0 * dev->y_step * dev_3d->y_step),
 		   				HEIGHT / 2 - ((tab_height - 1) / 2.0 * dev->x_step), 0.0);
 	*x_start = tmp_pnt->x - (((tab_width - 1) / 2.0) * dev->x_step);
 	*y_start = tmp_pnt->y - (((tab_width - 1) / 2.0) * dev->y_step);
@@ -188,9 +188,11 @@ t_vector	*get_3d_dev(t_mlx *mlx)
 
 	hypothenuse = sqrt(((mlx->pov->y - HEIGHT / 2) * (mlx->pov->y - HEIGHT / 2))
 						+ (mlx->pov->height * mlx->pov->height));
-	angle = sin(((double)mlx->pov->height) / hypothenuse * RAD);
+	angle = mlx->pov->height / hypothenuse;
+	printf("height : %f, hypo : %f\n", mlx->pov->height, hypothenuse);
+	angle = asin(angle) / RAD;
+	dev_3d = ft_new_vector(1.0, sin(angle * RAD));
 	printf("%f\n", angle);
-	dev_3d = ft_new_vector(1.0, asin(angle));
 	return (dev_3d);
 }
 
@@ -211,10 +213,11 @@ int			ft_render_mesh(void *mlx_param)
 	t_mlx		*mlx;
 	double		start_x;
 	double		start_y;
-	double	min;
+	double		min;
 
 	mlx = (t_mlx*)mlx_param;
-	mlx->pov->y = mlx->height * 2;
+	mlx->pov->y = mlx->height / 2 * 3;
+	mlx->pov->height -= 5;
 	//mlx->pov->zoom -= 0.005;
 	//mlx->pov->head_balance += 0.7;
 
@@ -222,24 +225,24 @@ int			ft_render_mesh(void *mlx_param)
 	
 	min = ft_get_min(mlx);
 	dev_3d = get_3d_dev(mlx);
-	dev = ft_new_vector(cos(mlx->pov->head_balance * RAD) * min * dev_3d->x_step, sin(mlx->pov->head_balance * RAD) * min * dev_3d->y_step);
-	ft_get_start_point(&start_x, &start_y, mlx, dev);
+	dev = ft_new_vector(cos(mlx->pov->head_balance * RAD) * min, sin(mlx->pov->head_balance * RAD) * min);
+	ft_get_start_point(&start_x, &start_y, mlx, dev, dev_3d);
 	y = 0;
 	while (mlx->mesh[y + 1])
 	{
 		x = 1;
 		current_x = start_x - (y * dev->y_step);
-		current_y = start_y + (y * dev->x_step);
+		current_y = start_y + (y * dev->x_step * dev_3d->y_step);
 		while (x < mlx->mesh[y][0])
 		{
 			ft_draw_rect(mlx, ft_new_rect(
 					ft_new_point(current_x, current_y, mlx->mesh[y][x]),
-					ft_new_point(current_x + dev->x_step, current_y + dev->y_step, mlx->mesh[y][x + 1]),
-					ft_new_point(current_x + dev->x_step - dev->y_step, current_y + dev->y_step + dev->x_step, mlx->mesh[y + 1][x + 1]),
-					ft_new_point(current_x - dev->y_step, current_y + dev->x_step, mlx->mesh[y + 1][x])
+					ft_new_point(current_x + dev->x_step, current_y + (dev->y_step * dev_3d->y_step), mlx->mesh[y][x + 1]),
+					ft_new_point(current_x + dev->x_step - (dev->y_step * dev_3d->y_step), current_y + ((dev->y_step + dev->x_step) * dev_3d->y_step), mlx->mesh[y + 1][x + 1]),
+					ft_new_point(current_x - (dev->y_step * dev_3d->y_step), current_y + (dev->x_step * dev_3d->y_step), mlx->mesh[y + 1][x])
 										), ft_get_color);
 			current_x += dev->x_step;
-			current_y += dev->y_step;
+			current_y += (dev->y_step * dev_3d->y_step);
 			x++;
 		}
 		y++;
