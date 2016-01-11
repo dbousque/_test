193c18eaedecaab8127227bcb78ad3c970f4e040
dodo <dbousque@student.42.fr>
2016-01-11 23:57:45 +0100

    lol

--- ft_ls/ft_ls.h
+++ ft_ls/ft_ls.h
@@ -18,12 +18,18 @@
 # include <sys/stat.h>
 # include <sys/ioctl.h>
 # include <sys/xattr.h>
+# include <sys/types.h>
 # include <errno.h>
 # include <stdio.h>
 # include <pwd.h>
 # include <uuid/uuid.h>
 # include <grp.h>
 # include <time.h>
+
+# define MINORBITS 20
+# define MINORMASK ((1U << MINORBITS) - 1)
+# define MAJOR(dev) ((unsigned int) ((dev) >> MINORBITS))
+# define MINOR(dev) ((unsigned int) ((dev) & MINORMASK))
 
 typedef struct	s_flags
 {
