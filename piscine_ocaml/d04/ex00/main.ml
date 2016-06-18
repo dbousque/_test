4baa9e6cce84d77bd87a4c6c0416bf2174d64f38
dodo <dbousque@student.42.fr>
2016-06-18 02:31:45 +0200

    almost done d03

---
+++ piscine_ocaml/d04/ex00/main.ml
@@ -0,0 +1,19 @@
+
+
+let print_color_list cols =
+	let rec print_cols cols func n =
+		if n = 0 then print_string "[" ;
+		match cols with
+			| [] -> print_string "]"
+			| col::cols ->
+				if n <> 0 then print_string ", " ;
+				print_string (func col) ;
+				print_cols cols func (n + 1)
+	in
+	print_cols cols Color.toString 0 ;
+	print_char '\n' ;
+	print_cols cols Color.toStringVerbose 0 ;
+	print_char '\n'
+
+let () =
+	print_color_list (Color.all ())
---
+++ piscine_ocaml/d04/ex01/main.ml
@@ -0,0 +1,43 @@
+
+
+let print_card_list cards func =
+	let rec print_cards cards func n =
+		if n = 0 then print_string "[" ;
+		match cards with
+			| [] -> print_string "]"
+			| card::cards ->
+				if n <> 0 then print_string ", " ;
+				print_string (func card) ;
+				print_cards cards func (n + 1)
+	in
+	print_cards cards func 0
+
+let rm_first = function
+	| [] -> []
+	| x::xs -> xs
+
+let rec rm_last = function
+	| [] -> []
+	| x::xs when xs = [] -> []
+	| x::xs -> x::(rm_last xs)
+
+let rec get_nexts = function
+	| [] -> []
+	| card::cards -> (Value.next card)::(get_nexts cards)
+
+let rec get_previous = function
+	| [] -> []
+	| card::cards -> (Value.previous card)::(get_previous cards)
+
+let () =
+	let nexts = get_nexts (rm_last (Value.all ())) in
+	let previous = get_previous (rm_first (Value.all ())) in
+	print_string "all cards : " ;
+	print_card_list (Value.all ()) Value.toString ;
+	print_string "\nall cards verbose : " ;
+	print_card_list (Value.all ()) Value.toStringVerbose ;
+	print_string "\nall valid nexts : " ;
+	print_card_list nexts Value.toString ;
+	print_string "\nall valid previous : " ;
+	print_card_list previous Value.toString ;
+	print_char '\n'
