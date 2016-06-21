4e6883831cb0bf3b1f6b522bb61c81819db12322
dodo <dodo@dodo-3.local>
2016-06-21 22:56:50 +0200

    finished ex03

--- piscine_ocaml/d06/ex03/ex03.ml
+++ piscine_ocaml/d06/ex03/ex03.ml
@@ -31,21 +31,40 @@ module type FIXED = sig
     val foreach : t -> t -> (t -> unit) -> unit
 end
 
-module FixedPoint : FIXED =
-    functor (Frac : FRACTIONNAL_BITS) ->
-        struct
-            type t = {bits: int ; point: int}
-
-            let of_float f = {bits = int_of_float (f *. 100.0) ; point = 2}
-        end
-
 module type MAKE =
     functor (Frac : FRACTIONNAL_BITS) ->
         FIXED
 
 module Make : MAKE =
     functor (Frac : FRACTIONNAL_BITS) ->
-        FixedPoint
+        struct
+            type t = int
+
+            let of_float i =
+                let round v = if (ceil v) -. v < v -. (floor v) then ceil v else floor v in
+                int_of_float (round (i *. (float_of_int (1 lsl Frac.bits))))
+            let of_int i = i lsl Frac.bits
+            let to_float e = (float_of_int e) /. float_of_int (1 lsl Frac.bits)
+            let to_int e = e lsl Frac.bits
+            let to_string e = string_of_float (to_float e)
+            let zero = 0
+            let one = of_int 1
+            let succ e = e + 1
+            let pred e = e - 1
+            let min e1 e2 = if e1 <= e2 then e1 else e2
+            let max e1 e2 = if e1 >= e2 then e1 else e2
+            let gth e1 e2 = e1 > e2
+            let lth e1 e2 = e1 < e2
+            let gte e1 e2 = e1 >= e2
+            let lte e1 e2 = e1 <= e2
+            let eqp e1 e2 = e1 = e2
+            let eqs e1 e2 = e1 == e2
+            let add e1 e2 = e1 + e2
+            let sub e1 e2 = e1 - e2
+            let mul e1 e2 = (e1 * e2 + (1 lsl (Frac.bits - 1))) lsr Frac.bits
+            let div e1 e2 = ((e1 lsl Frac.bits) + (e2 / 2)) / e2
+            let rec foreach e1 e2 func = if lte e1 e2 then (func e1 ; foreach (succ e1) e2 func)
+        end
 
 module Fixed4 : FIXED = Make (struct let bits = 4 end)
 module Fixed8 : FIXED = Make (struct let bits = 8 end)
