590e4997ed3a64751949b932a6ba261f17e4f08f
dodo <dbousque@student.42.fr>
2017-12-09 19:50:54 +0100

    handle connection error

--- rendu/ft_irc/bot.py
+++ rendu/ft_irc/bot.py
@@ -6,26 +6,33 @@ import requests as r
 import re
 
 def get_recast_response(user, inp):
-  headers = { 'Authorization': 'Token c46ba906e94f1bf22599982dc29831b4' }
+  headers = { 'Authorization': 'Token de54ffec9b29cd365cefc914477b4dfc' }
   msg = { 'attachment': { 'type': 'text', 'content': inp } }
   json = { 'message': msg, 'conversation_id': user }
   res = r.post('https://api.recast.ai/build/v1/dialog', headers=headers, json=json).json()
   if len(res['results']['messages']) > 0:
     return res['results']['messages'][0]['content']
-  return 'I don\'t know how to answer'
+  return 'Je ne sais pas comment repondre'
 
-if len(sys.argv) < 2:
-  print('Usage : {} <port>'.format(sys.argv[0]))
-  sys.exit(1)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', int(sys.argv[1])))
-s.recv(4096)
-s.send(b'/nick bot\r\n')
-while True:
-  inp = s.recv(4096)
-  inp = inp.decode('utf-8')
-  if re.match('^\'.+?\' > ', inp):
-    user = ''.join(inp.split('>', 1)[0].split('\'')[1:-1])
-    inp = inp.split('>', 1)[1].strip().replace('\r', '').replace('\n', '')
-    resp = get_recast_response(user, inp)
-    s.send('/msg {} :{}\r\n'.format(user, resp).encode('utf-8'))
+try:
+  if len(sys.argv) < 2:
+    print('Usage : {} <port>'.format(sys.argv[0]))
+    sys.exit(1)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  try:
+    s.connect(('127.0.0.1', int(sys.argv[1])))
+  except:
+    print('Could not connect')
+    sys.exit(1)
+  s.recv(4096)
+  s.send(b'/nick bot\r\n')
+  while True:
+    inp = s.recv(4096)
+    inp = inp.decode('utf-8')
+    if re.match('^\'.+?\' > ', inp):
+      user = ''.join(inp.split('>', 1)[0].split('\'')[1:-1])
+      inp = inp.split('>', 1)[1].strip().replace('\r', '').replace('\n', '')
+      resp = get_recast_response(user, inp)
+      s.send('/msg {} :{}\r\n'.format(user, resp).encode('utf-8'))
+except:
+  print('error')
